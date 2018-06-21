cf9bc6acb61708c84568b5ff3b105d48d65dce66
stealthydonut <kinger6688@gmail.com>
2018-06-21 13:36:35 -0400

    Update gold_yield_compare.py

--- gold_yield_compare.py
+++ gold_yield_compare.py
@@ -10,6 +10,7 @@ import StringIO
 import quandl
 import datetime as dt
 import ast
+import pandas as pd
 import itertools
 #import matplotlib
 #import matplotlib.pyplot as plt
@@ -34,30 +35,44 @@ fred = Fred(api_key='4af3776273f66474d57345df390d74b6')
 #############################################
 
 yield_list=[
-('2-Year High Quality Market (HQM) Corporate Bond Par Yield','HQMCB2YRP'),
-('5-Year High Quality Market (HQM) Corporate Bond Par Yield','HQMCB5YRP'),
-('10-Year High Quality Market (HQM) Corporate Bond Par Yield','HQMCB10YRP'),
-('30-Year High Quality Market (HQM) Corporate Bond Par Yield','HQMCB30YRP')]
+('2Year Corp Yield','HQMCB2YRP'),
+('5Year Corp Yield','HQMCB5YRP'),
+('10Year Corp Yield','HQMCB10YRP'),
+('30Year Corp Yield','HQMCB30YRP')]
 
 quandl_list=['gold','LBMA/GOLD']
 
 #Get the Fred data
-
 freddata = pd.DataFrame()
 fredfile = pd.DataFrame()
+compriceslast = pd.DataFrame()
+
 
 for i in yield_list:     
-    try:
-        fredvalue=''.join(i[1])
-        fredname=''.join(i[0])
-        freddata = fred.get_series_all_releases(fredvalue)
-        freddata['price']=pd.to_numeric(freddata['value'], errors='coerce')
-        freddata['variable'] = fredname
+    fredvalue=''.join(i[1])
+    fredname=''.join(i[0])
+    freddata = fred.get_series_all_releases(fredvalue)
+    freddata[fredname]=pd.to_numeric(freddata['value'], errors='coerce')
+    freddata['ind']=freddata['date']
+    freddata.__delitem__('value')
+    freddata.__delitem__('realtime_start')
+    freddata.__delitem__('date')
+    fredfile = pd.merge(fredfile, freddata, left_on='ind', right_on='ind')  
+#Done
+
+
+    
+    
+    
+    
         freddata['ind']=freddata['date']
         freddata.__delitem__('value')
-        fredfile = fredfile.append(freddata, ignore_index=False)        
+        fredfile = pd.merge(fredfile, freddata, left_on='ind', right_on='ind')        
     except:
         print
+
+        bigdata2=pd.merge(all_data_dataset, bigdata, left_on='monthyear', right_on='monthyear')
+        
         
 com_list=fredfile.drop_duplicates(['variable'], keep='last')
 dflist = com_list['variable'].tolist()
@@ -69,15 +84,15 @@ com_list2=fredfile.sort_values(['variable','ind'])
 for i in dflist:  
     com_name=''.join(i) 
     #only include the current commodity 
-    df = fredfile[fredfile['variable']==com_name]            
+    df = com_list2[com_list2['variable']==com_name]            
     #Generate the calculations
-    df['lag125'] = df['price'].shift(125)
-    df['lag250'] = df['price'].shift(250)
-    df['lag500'] = df['price'].shift(500)
-    df['lag750'] = df['price'].shift(750)
-    df['lag1000'] = df['price'].shift(1000)
-    df['lag1250'] = df['price'].shift(1250)
-    df['lag1500'] = df['price'].shift(1500)
+    df['lag125'] = df['price'].shift(6)
+    df['lag250'] = df['price'].shift(12)
+    df['lag500'] = df['price'].shift(24)
+    df['lag750'] = df['price'].shift(36)
+    df['lag1000'] = df['price'].shift(48)
+    df['lag1250'] = df['price'].shift(60)
+    df['lag1500'] = df['price'].shift(72)
     df['maxalltime'] = df['price'].max()
     df['minalltime'] = df['price'].min()
     #do the five year max and min
@@ -105,32 +120,52 @@ for i in dflist:
     #comprices = comprices.append(df, ignore_index=False)
     compriceslast = compriceslast.append(lastrow, ignore_index=False)
 
-print compriceslast
-
-
- 
+####################
 #Get the quandl data
+####################
 
-quandlraw = pd.DataFrame()
+df= quandl.get('LBMA/GOLD')
+df.columns=['price','EUR AM','GBP AM','USD PM','EUR PM','GBP PM']
+df['variable']='gold'
+df.__delitem__('EUR AM')
+df.__delitem__('GBP AM')
+df.__delitem__('USD PM')
+df.__delitem__('EUR PM')
+df.__delitem__('GBP PM') 
+df['ind']=dflmba.index
+df['lag125'] = df['price'].shift(125)
+df['lag250'] = df['price'].shift(250)
+df['lag500'] = df['price'].shift(500)
+df['lag750'] = df['price'].shift(750)
+df['lag1000'] = df['price'].shift(1000)
+df['lag1250'] = df['price'].shift(1250)
+df['lag1500'] = df['price'].shift(1500)
+df['maxalltime'] = df['price'].max()
+df['minalltime'] = df['price'].min()
+#do the five year max and min
+lastrow1250=df.tail(1250)
+lastrow1250['max5year'] = lastrow1250['price'].max()
+lastrow1250['min5year'] = lastrow1250['price'].min()    
+#do the three year max and min
+lastrow750=lastrow1250.tail(750)
+lastrow750['max3year'] = lastrow750['price'].max()
+lastrow750['min3year'] = lastrow750['price'].min()
+#Gets the final row of data
+lastrow=lastrow750.tail(1)
+#Calculate percentages
+lastrow['6mochange']=(lastrow['price']/lastrow['lag125'])-1
+lastrow['1yrchange']=(lastrow['price']/lastrow['lag250'])-1
+lastrow['2yrchange']=(lastrow['price']/lastrow['lag500'])-1
+lastrow['3yrchange']=(lastrow['price']/lastrow['lag750'])-1
+lastrow['4yrchange']=(lastrow['price']/lastrow['lag1000'])-1
+lastrow['5yrchange']=(lastrow['price']/lastrow['lag1250'])-1
+lastrow['5yrmaxdiff']=(lastrow['price']/lastrow['max5year'])-1 
+lastrow['5yrmindiff']=(lastrow['price']/lastrow['min5year'])-1 
+lastrow['3yrmaxdiff']=(lastrow['price']/lastrow['max3year'])-1     
+lastrow['3yrmindiff']=(lastrow['price']/lastrow['min3year'])-1 
 
-for i in quandl_list:
-    commodity=''.join(i[0]) 
-    value=''.join(i[1]) 
-    dflmba= quandl.get(value)
-    dflmba.columns=['price','EUR AM','GBP AM','USD PM','EUR PM','GBP PM']
-    dflmba['commodity']=commodity
-    dflmba.__delitem__('EUR AM')
-    dflmba.__delitem__('GBP AM')
-    dflmba.__delitem__('USD PM')
-    dflmba.__delitem__('EUR PM')
-    dflmba.__delitem__('GBP PM') 
-    dflmba['ind']=dflmba.index
-    quandlraw = quandlraw.append(dflmba, ignore_index=False)
 
-
-lmba=quandlraw.append(fredfile, ignore_index=True)
-
-print fredfile
+compriceslast = compriceslast.append(lastrow, ignore_index=False)
 
 ###############################################################################################################
 #constuct moving averages to determine if the yield is higher than the gold return over the same period of time
