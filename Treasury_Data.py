b1469dcd107caa24f9fb60434018c09393937057
stealthydonut <kinger6688@gmail.com>
2018-08-12 16:52:48 -0400

    Update Treasury_Data.py

--- Treasury_Data.py
+++ Treasury_Data.py
@@ -1,4 +1,8 @@
 #Source is from https://github.com/npezolano/PyTreasuryDirect/blob/master/README.md#
+#requirs CUSIP, and ISSUE Date
+#list of all identifiers for CUSIP https://www.treasurydirect.gov/instit/auctfund/work/auctime/auctime_securitiestable.htm
+#the date is the issue date of the cusip number
+
 from pytreasurydirect import TreasuryDirect
 
 td = TreasuryDirect()
