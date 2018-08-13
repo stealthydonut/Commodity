7e63b0aa7bd6da4f44ec23a1ca60a86d02af024a
stealthydonut <kinger6688@gmail.com>
2018-08-13 13:11:57 -0400

    Update Treasury_Data.py

--- Treasury_Data.py
+++ Treasury_Data.py
@@ -11,3 +11,24 @@ cusip = '912796CJ6'
 print(td.security_info(cusip, '02/11/2014'))
 # or 
 print(td.security_info(cusip, datetime.date(2014, 2, 11)))
+
+from pytreasurydirect import TreasuryDirect
+
+td = TreasuryDirect()
+cusip = '912796CJ6'
+2018-08-09
+
+cusip='912796QW2'
+test=(td.security_info(cusip, '08/09/2018'))
+
+for i in dateval:
+    test=(td.security_info(cusip, '08/14/2018'))
+
+912796QV4
+#Make a dictionary file#
+
+for i in dateval:
+    test=(td.security_info(cusip, '02/11/2014'))
+#
+pd.DataFrame(test.items())
+df = pd.DataFrame(test, index=['a'])
