926b0295bb52b97de2edd3b1969601c4b40d92e9
stealthydonut <kinger6688@gmail.com>
2018-08-14 08:54:32 -0400

    Update Treasury_Data.py

--- Treasury_Data.py
+++ Treasury_Data.py
@@ -5,6 +5,7 @@
 #for a list of cusips and issue dates https://www.treasurydirect.gov/govt/reports/pd/mspd/2010/2010_jan.htm - this can be downloaded
 #in excel form
 import pandas as pd
+import pandas as pd
 from pytreasurydirect import TreasuryDirect
 
 td = TreasuryDirect()
@@ -15,6 +16,11 @@ for i in cusip_list:
     cusip =''.join(i[0]) 
     issuedate =''.join(i[1])
     cusip_value=(td.security_info(cusip, issuedate))
-    pd.DataFrame(cusip_value.items())
-    df = pd.DataFrame(cusip_value, index=['a'])   
-#
+    cusip_value = [str(r) for r in cusip_value]
+    cusip_value=['curr_outstanding']=pd.to_numeric(cusip_value=['currentlyOutstanding'], errors='coerce')
+    cusip_value['issue_date']=pd.to_datetime(data2['issuedate'], errors='coerce')
+    df=
+    #pd.DataFrame(cusip_value.items())
+    df = pd.DataFrame(cusip_value, index=['a']) 
+    td = td.append(df, ignore_index=False)
+
